12607ee554cfe8b5afe66af50905cd74664a9690
fukayatsu <fukayatsu@gmail.com>
2014-12-02 00:14:18 +0900

    Add spec with multiple versions

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -4,7 +4,7 @@ require File.expand_path('../boot', __FILE__)
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "active_resource/railtie"
+require "active_resource/railtie" if Rails::VERSION::MAJOR < 4
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
@@ -59,4 +59,3 @@ module Dummy
     config.assets.version = '1.0'
   end
 end
-
